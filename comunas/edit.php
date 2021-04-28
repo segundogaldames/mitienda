e56647ba4b6044036ea6dcd59a64877bdb84802d
Segundo Galdames <segundogaldames@gmail.com>
2021-04-28 17:25:03 -0300

    Implementacion de edicion de comunas - completo

--- comunas/edit.php
+++ comunas/edit.php
@@ -23,35 +23,26 @@ if (isset($_GET['id'])) {
        
         //guardamos y sanitizamos la variable nombre
         $nombre = trim(strip_tags($_POST['nombre']));
+        $region = (int) $_POST['region'];
 
         if (!$nombre) {
             $msg = 'Ingrese el nombre de la comuna';
-        }else{
-            //verificar que la comuna ingresada no exista
-            $res = $mbd->prepare("SELECT id FROM comunas WHERE nombre = ?");
+        }elseif($region <= 0){
+            $msg = 'Seleccione una región';
+        }else {
+            //procedemos a actualizar los datos de la tabla comunas segun id
+            $res = $mbd->prepare("UPDATE comunas SET nombre = ?, region_id = ?, updated_at = now() WHERE id = ?");
             $res->bindParam(1, $nombre);
+            $res->bindParam(2, $region);
+            $res->bindParam(3, $id);
             $res->execute();
 
-            $comuna = $res->fetch();
+            $row = $res->rowCount();
 
-            if ($comuna) {
-                $msg = 'La comuna ingresada ya existe... intente con otra';
-            }else{
-
-                //guardamos los datos en la tabla comunas
-                $res = $mbd->prepare("INSERT INTO comunas VALUES(null, ?, ?, now(), now() )");
-                $res->bindParam(1, $nombre);
-                $res->bindParam(2, $id_region);
-                $res->execute();
-
-                //rescatar el numero de filas afectadas
-                $row = $res->rowCount();
-
-                if ($row) {
-                    $msg = 'ok';
-                    header('Location: index.php?m=' . $msg);
-                }
-            } 
+            if ($row) {
+                $msg = 'ok';
+                header('Location: show.php?id=' . $id . '&m=' . $msg);
+            }
         }
    }
 }
@@ -65,7 +56,7 @@ if (isset($_GET['id'])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Regiones</title>
+    <title>Comunas</title>
     <!--Enlaces CDN de Bootstrap-->
     <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script> -->
@@ -85,7 +76,7 @@ if (isset($_GET['id'])) {
         <section>
 
             <div class="col-md-6 offset-md-3">
-                <h1>Nueva Comuna</h1>
+                <h1>Editar Comuna</h1>
 
                 <!-- mensajes de validacion y errores -->
                 <?php if(isset($msg)): ?>
@@ -94,32 +85,36 @@ if (isset($_GET['id'])) {
                     </p>
                 <?php endif; ?>
 
-                <form action="" method="post">
-                    <div class="form-group mb-3">
-                        <label for="">Comuna <span class="text-danger">*</span></label>
-                        <input type="text" name="nombre" value="<?php echo $comuna['comuna']; ?>" class="form-control" placeholder="Ingrese el nombre de la comuna">
-                    </div>
-                    <div class="form-group mb-3">
-                        <label for="">Región <span class="text-danger">*</span></label>
-                        <select name="region" class="form-control">
-                            <option value="<?php echo $comuna['region_id']; ?>">
-                                <?php echo $comuna['region']; ?>
-                                </option>
+                <?php if($comuna): ?>
+                    <form action="" method="post">
+                        <div class="form-group mb-3">
+                            <label for="">Comuna <span class="text-danger">*</span></label>
+                            <input type="text" name="nombre" value="<?php echo $comuna['comuna']; ?>" class="form-control" placeholder="Ingrese el nombre de la comuna">
+                        </div>
+                        <div class="form-group mb-3">
+                            <label for="">Región <span class="text-danger">*</span></label>
+                            <select name="region" class="form-control">
+                                <option value="<?php echo $comuna['region_id']; ?>">
+                                    <?php echo $comuna['region']; ?>
+                                    </option>
 
-                            <?php foreach($regiones as $region): ?>
-                                <option value="<?php echo $region['id']; ?>">
-                                    <?php echo $region['nombre']; ?>
-                                </option>
-                            <?php endforeach; ?>
-                        
-                        </select>
-                    </div>
-                    <div class="form-group mb-3">
-                        <input type="hidden" name="confirm" value="1">
-                        <button type="submit" class="btn btn-primary">Guardar</button>
-                        <a href="show.php?id=<?php echo $id; ?>" class="btn btn-link">Volver</a>
-                    </div>
-                </form>
+                                <?php foreach($regiones as $region): ?>
+                                    <option value="<?php echo $region['id']; ?>">
+                                        <?php echo $region['nombre']; ?>
+                                    </option>
+                                <?php endforeach; ?>
+                            
+                            </select>
+                        </div>
+                        <div class="form-group mb-3">
+                            <input type="hidden" name="confirm" value="1">
+                            <button type="submit" class="btn btn-primary">Guardar</button>
+                            <a href="show.php?id=<?php echo $id; ?>" class="btn btn-link">Volver</a>
+                        </div>
+                    </form>
+                <?php else: ?>
+                    <p class="text-info">El dato no existe</p>
+                <?php endif; ?>
             </div>
             
 
